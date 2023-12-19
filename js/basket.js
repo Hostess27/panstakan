324c6b1b7db2f786a002b1dfdaa358257549489f
Hostess27 <pash.pash22173@gmail.com>
2023-12-19 22:27:16 +0100

    add links for all pages

--- js/basket.js
+++ js/basket.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Получаем ссылку на корзину и счетчик
-    const basketLink = document.querySelector('.header__basket a');
-    const basketCounter = document.querySelector('.basket-counter');
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Получаем ссылку на корзину и счетчик
+//     const basketLink = document.querySelector('.header__basket a');
+//     const basketCounter = document.querySelector('.basket-counter');
 
-    // Имитация данных о количестве товаров в корзине (можете заменить на реальные данные)
-    let itemCount = 0;
+//     // Имитация данных о количестве товаров в корзине (можете заменить на реальные данные)
+//     let itemCount = 0;
 
-    // Обновляем счетчик при загрузке страницы
-    updateBasketCounter();
+//     // Обновляем счетчик при загрузке страницы
+//     updateBasketCounter();
 
-    // Функция для обновления счетчика
-    function updateBasketCounter() {
-        basketCounter.textContent = itemCount === 0 ? '+' : itemCount;
-    }
+//     // Функция для обновления счетчика
+//     function updateBasketCounter() {
+//         basketCounter.textContent = itemCount === 0 ? '+' : itemCount;
+//     }
 
-    // Можно вызывать эту функцию, когда пользователь добавляет товар в корзину
-    function addItemToBasket() {
-        itemCount++;
-        updateBasketCounter();
-    }
+//     // Можно вызывать эту функцию, когда пользователь добавляет товар в корзину
+//     function addItemToBasket() {
+//         itemCount++;
+//         updateBasketCounter();
+//     }
 
-    // Пример вызова функции при клике на корзину (вы можете использовать свою логику)
-    basketLink.addEventListener('click', function (event) {
-        event.preventDefault();
-        // Здесь может быть логика перехода на страницу корзины
-    });
-});
+//     // Пример вызова функции при клике на корзину (вы можете использовать свою логику)
+//     basketLink.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         // Здесь может быть логика перехода на страницу корзины
+//     });
+// });
